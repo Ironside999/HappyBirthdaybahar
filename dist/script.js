ce170c5fab45688d0e79bdb63fdab1403656def4
m.esfandiari <mohammadesfandiari890@gmail.com>
2022-02-22 13:45:50 +0330

    Change img

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,8 @@
 
 const button = document.getElementById('audio');
 // button.play();
-
+console.log(button);
+log(document.body)
 document.body.addEventListener('load', () => {
     button.play();
 })
